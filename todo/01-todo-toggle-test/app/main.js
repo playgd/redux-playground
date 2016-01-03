ce6c14ee70c07478ceb016c4d4f22a9a7350eb71
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-03 18:23:42 -0200

    Todo Add test to toggle todo

---
+++ todo/01-todo-toggle-test/app/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
+
+const toggleTodo = (todo) => {
+
+};
+
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true
+  };
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  };
+
+  expect(
+    toggleTodo(todoBefore)    
+  ).toEqual(todoAfter);
+};
+
+testToggleTodo();
+console.log('All tests passed!');
