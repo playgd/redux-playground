b30d8e0ee31bae0bcf0cbabb5db05bc569d25437
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-03 20:16:52 -0200

    Todo Add reducer visibilityFilter

--- todo/03-composition-with-objects/app/main.js
+++ todo/03-composition-with-objects/app/main.js
@@ -37,6 +37,18 @@ const todos = (state = [], action) => {
   }
 };
 
+const visibilityFilter = (
+  state = 'SHOW_ALL',
+  action
+) => {
+  switch(action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      state;
+  }
+};
+
 const store = createStore(todos);
 
 console.log('Initial state:');
