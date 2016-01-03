dd1d2dc6132de741a40476d89c61754271330944
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-03 18:18:41 -0200

    Todo Add files to toggle test

---
+++ todo/01-todo-toggle-test/webpack.config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  entry: './app/main.js',
+  output: {
+    filename: 'public/bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
