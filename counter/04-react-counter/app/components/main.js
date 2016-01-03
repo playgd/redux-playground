466ddbed9563aec531d1919e66d47a310170e805
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-03 14:45:11 -0200

    Counter React Add events to increment and decrement

--- counter/04-react-counter/app/components/main.js
+++ counter/04-react-counter/app/components/main.js
@@ -5,16 +5,30 @@ import ReactDOM from 'react-dom';
 import store from '../store';
 
 const Counter = ({
-  value
+  value,
+  onIncrement,
+  onDecrement
 }) => {
   return (
-    <h1>{value}</h1>    
+    <div>
+      <h1>{value}</h1>
+      <button onClick={onDecrement}>-</button>
+      <button onClick={onIncrement}>+</button>
+    </div>
   );
 };
 
 const render = () => {
   ReactDOM.render(
-    <Counter value={store.getState()} />,
+    <Counter 
+      value={store.getState()}
+      onIncrement={() =>
+        store.dispatch({ type: 'INCREMENT' })
+      }
+      onDecrement={() =>
+        store.dispatch({ type: 'DECREMENT' })
+      }
+    />,
     document.getElementById('app')  
   );
 };
