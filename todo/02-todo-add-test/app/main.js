fc0c060bc5de829b6df1ab3859474e1e4c544b04
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-03 19:12:36 -0200

    Todo Test to add todo pass.

--- todo/02-todo-add-test/app/main.js
+++ todo/02-todo-add-test/app/main.js
@@ -4,7 +4,19 @@ import expect from 'expect';
 import deepFreeze from 'deep-freeze';
 
 const todos = (state = [], action) => {
-
+  switch(action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ];
+    default:
+      return state;
+  }
 };
 
 const testAddTodo = () => {
