5d53af54cfe70dac381dacc984367c07248409d3
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-03 17:21:53 -0200

    Add / Remove counter tests Update dependencies and add loader to use es2015

--- counter/05-add-counter-test/webpack.config.js
+++ counter/05-add-counter-test/webpack.config.js
@@ -10,7 +10,10 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel'
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
       }  
     ]
   }
