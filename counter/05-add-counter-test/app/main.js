ab2b90b306b276caec49922ad33e3a6190284da3
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-03 17:26:34 -0200

    Add / Remove counter tests Add test to add counter. This test fail

--- counter/05-add-counter-test/app/main.js
+++ counter/05-add-counter-test/app/main.js
@@ -1,2 +1,20 @@
 'use strict';
 
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
+
+const addCounter = (list) => {
+
+};
+
+const testAddCounter = () => {
+  const listBefore = [];
+  const listAfter = [0];
+
+  expect(
+    addCounter(listBefore)
+  ).toEqual(listAfter);
+};
+
+testAddCounter();
+console.log('All tests passed!');
