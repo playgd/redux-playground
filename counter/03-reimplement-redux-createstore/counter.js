2e0d24bac9702ab52077b57b01d7f98942d09d5a
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-03 12:58:24 -0200

    Counter Reimplement createStore method from Redux

---
+++ counter/03-reimplement-redux-createstore/counter.js
@@ -0,0 +1,32 @@
+(() => {
+  'use strict';
+
+  const counter = (state = 0, action) => {
+    switch(action.type) {
+      case 'INCREMENT':
+        return state + 1;
+      case 'DECREMENT':
+        return state - 1;
+      default:
+        return state;
+    } 
+  };
+
+  const createStore = (reducer) => {
+  
+  };
+
+  const store = createStore(counter);
+
+  const render = () => {
+    document.body.innerText = store.getState();
+  };
+  
+  store.subscribe(render);
+  render();
+
+  document.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREMENT' });
+  }, false);
+})();
+
