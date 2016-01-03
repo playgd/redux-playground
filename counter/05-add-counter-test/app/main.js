95703704859ddc3af2553e93d20eca1321d367d0
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-03 18:00:17 -0200

    Add / Remove counter tests Refactory: using spread operator to improve visibility

--- counter/05-add-counter-test/app/main.js
+++ counter/05-add-counter-test/app/main.js
@@ -15,10 +15,11 @@ const removeCounter = (list, index) => {
 };
 
 const incrementCounter = (list, index) => {
-  return list
-    .slice(0, index)
-    .concat(list[index] + 1)
-    .concat(list.slice(index + 1));
+  return [
+    ...list.slice(0, index),
+    list[index] + 1,
+    ...list.slice(index + 1)
+  ];
 };
 
 const testAddCounter = () => {
