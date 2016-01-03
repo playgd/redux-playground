2b5122b4314b24a984814b0026c416698b0a9348
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-03 10:38:10 -0200

    Counter All tests passed returning current state

--- counter/counter-with-tests/counter.js
+++ counter/counter-with-tests/counter.js
@@ -3,8 +3,12 @@
 function counter(state, action) {
   if(action.type === 'INCREMENT') {
     return state + 1;
-  } else if(action.type === 'DECREMENT') {
+  } 
+  else if(action.type === 'DECREMENT') {
     return state -1;
+  }
+  else {
+    return state;
   }
 }
 
