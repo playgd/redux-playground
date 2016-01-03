7bb60e4fc8d45b0b3d29f869d2932acf0482a280
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-03 14:30:31 -0200

    Counter React Add counter reducer

---
+++ counter/04-react-counter/app/reducers/counter.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import { createStore } from 'redux';
+
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  } 
+};
+
+const store = createStore(counter);
+
+export default store;
