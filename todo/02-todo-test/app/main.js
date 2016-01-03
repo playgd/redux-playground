698e2dec2f1ab3e45b3c8a7cb4dd8cae5d2649db
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-03 19:28:21 -0200

    Todo Add test to toggle todo

--- todo/02-todo-test/app/main.js
+++ todo/02-todo-test/app/main.js
@@ -42,5 +42,44 @@ const testAddTodo = () => {
   ).toEqual(stateAfter);
 };
 
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: false
+    } 
+  ];
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: true 
+    } 
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)    
+  ).toEqual(stateAfter);
+};
+
 testAddTodo();
+testToggleTodo();
 console.log('All tests passed!');
