a6025ec898d44bc0a63478e6b8a66ad4a9da0598
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-03 14:16:19 -0200

    Counter React Add hello world component

---
+++ counter/04-react-counter/app/components/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Main = () => {
+  return (
+    <h1>Hello World</h1>    
+  );
+};
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('app')  
+);
