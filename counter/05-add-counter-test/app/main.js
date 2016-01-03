73bbf696767da0eaa0801c72125745a5b9ed36a3
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-03 17:41:59 -0200

    Add / Remove counter tests Test to remove fails, because state can't be changed.

--- counter/05-add-counter-test/app/main.js
+++ counter/05-add-counter-test/app/main.js
@@ -27,6 +27,8 @@ const testRemoveCounter = () => {
   const listBefore = [0, 10, 20];
   const listAfter = [0, 20];
 
+  deepFreeze(listBefore);
+
   expect(
     removeCounter(listBefore, 1)
   ).toEqual(listAfter);
