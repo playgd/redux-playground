74dd25efc923e28b59e0c537e02f210cd6744d64
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-03 17:12:18 -0200

    Add / Remove counter tests Add files

---
+++ counter/05-add-counter-test/webpack.config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  entry: './app/main.js',
+  output: {
+    filename: 'public/bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }  
+    ]
+  }
+};
