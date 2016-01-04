5e763ec8c1e02a5502e6fc6e61239935319cb6bd
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-04 21:02:01 -0200

    Todo React Pass store down as props

--- todo/05-react-todo/app/components/todo-app.js
+++ todo/05-react-todo/app/components/todo-app.js
@@ -5,12 +5,12 @@ import AddTodo from './add-todo';
 import VisibleTodoList from './visible-todo-list';
 import Footer from './footer';
 
-const TodoApp = () => {
+const TodoApp = ({ store }) => {
   return (
     <div>
-      <AddTodo />
-      <VisibleTodoList />
-      <Footer />
+      <AddTodo store={store} />
+      <VisibleTodoList store={store} />
+      <Footer store={store} />
     </div>
   );
 };
--- todo/05-react-todo/app/main.js
+++ todo/05-react-todo/app/main.js
@@ -3,8 +3,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoApp from './components/todo-app';
+import store from './store';
 
 ReactDOM.render(
-  <TodoApp />,
+  <TodoApp store={store} />,
   document.querySelector('[data-js="app"]')
 );
