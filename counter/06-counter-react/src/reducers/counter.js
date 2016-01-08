fad3274435a7597751e8dfb9b849179798984511
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-08 08:46:42 -0200

    Improve reducer

--- counter/06-counter-react/src/reducers/counter.js
+++ counter/06-counter-react/src/reducers/counter.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const counter = (state = 0, action) => {
-  switch(action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  } 
+  const actions = {
+    INCREMENT: state + 1,
+    DECREMENT: state - 1
+  };
+
+  return (actions[action.type] !== undefined)
+    ? actions[action.type]
+    : state;
 };
 
 export default counter;
